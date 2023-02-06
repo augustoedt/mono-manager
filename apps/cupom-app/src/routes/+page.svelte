190cca22b0c3d30f454ad8e49c73ad5e839c2b0a
Augusto Eduardo <augustoedt@gmail.com>
2023-02-06 20:12:00 -0300

    fix prisma-orm import

---
+++ apps/cupom-app/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { MyCounterButton } from 'ui';
+    import type { PageData } from './$types';
+  export let data: PageData;
+</script>
+
+<h1>Cupom App</h1>
+<MyCounterButton />
+{#if data.users.length === 0}
+  <p>No users found</p>
+{:else}
+  {#each data.users as user}
+    <ul>
+      <li>{user.name}</li>
+    </ul>
+  {/each}
+{/if}
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,7 +2,6 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'turbo', 'prettier'],
   plugins: ['svelte3', '@typescript-eslint'],
-  ignorePatterns: ['*.cjs'],
   overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
   settings: {
     'svelte3/typescript': () => require('typescript')
