39db1089b2a6aa16759c8f3374934eb7693cf5d0
Augusto Eduardo <augustoedt@gmail.com>
2023-02-06 21:02:47 -0300

    fix gitignore

--- apps/cupom-app/.vercel/output/static/_app/immutable/chunks/1-e5dac745.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-165e6d44.js";export{t as component};
--- apps/cupom-app/.vercel/output/static/_app/immutable/chunks/2-4981c2df.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-c804c14b.js";export{t as component};
