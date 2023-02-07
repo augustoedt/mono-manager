1c5396c837c3e0d2f5a57804eef1b34b5fe73e1e
Augusto Eduardo <augustoedt@gmail.com>
2023-02-07 04:09:11 -0300

    fixing deploy error 1

--- packages/prisma-orm/src/client.ts
+++ packages/prisma-orm/src/client.ts
@@ -1,11 +1,2 @@
 import { PrismaClient } from "./lib/prisma";
-
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-export const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
-
 export * from "./lib/prisma";
